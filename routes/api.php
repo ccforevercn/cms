0a82503707a1440ff509f6787c316474371022a0
cc_forever <1253705861@qq.com>
2020-07-27 17:05:14 +0800

    '修改后台前缀调用方式'

--- routes/api.php
+++ routes/api.php
@@ -10,7 +10,7 @@
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-$admin = env('ADMIN_PREFIX', 'ccforever'); // 后台路由前缀
+$admin = config('ccforever.ADMIN_PREFIX', 'ccforever'); // 后台路由前缀
 Route::group(['prefix'=> $admin], function() use($admin) { // 后台路由组
     Route::post('/login', 'LoginController@login')->name('login'.$admin);// 登陆
     Route::get('/captcha', 'PublicController@captcha')->name('captcha'.$admin);// 验证码
