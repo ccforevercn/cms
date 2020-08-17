d65b0343a979f6f121b35958e9771d705a522d90
cc_forever <1253705861@qq.com>
2020-08-17 14:48:22 +0800

    '添加合作伙伴表'

---
+++ database/migrations/2020_08_17_064625_create_partners_table.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * @author: cc_forever<1253705861@qq.com>
+ * @day: 2020/8/17
+ */
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+/**
+ * 合作伙伴表
+ *
+ * Class CreatePartnersTable
+ */
+class CreatePartnersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('partners', function (Blueprint $table) {
+            $table->engine = 'InnoDB'; // 表存储引擎
+            $table->charset = 'utf8';  //表默认字符集
+            $table->collation = 'utf8_general_ci';  // 表默认的排序规则
+            $table->bigIncrements('id')->comment('合作伙伴表');
+            $table->string('name', 128)->comment('合作伙伴');
+            $table->string('link', 128)->comment('合作伙伴链接');
+            $table->string('image', 128)->comment('合作伙伴图片');
+            $table->integer('weight')->comment('合作伙伴权重');
+            $table->integer('add_time')->comment('添加时间');
+            $table->tinyInteger('is_del')->comment('是否删除(1是 0否)');
+            $table->unique('id'); // 编号唯一索引
+            $table->index('weight'); // 合作伙伴权重普通索引
+            $table->index('is_del'); // 是否删除普通索引
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('partners');
+    }
+}
