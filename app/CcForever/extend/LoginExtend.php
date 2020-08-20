7504e1620219837452448717d1d03e19ef17c4cd
cc_forever <1253705861@qq.com>
2020-08-20 08:23:38 +0800

    '修改登陆中间件验证流程'

--- app/CcForever/extend/LoginExtend.php
+++ app/CcForever/extend/LoginExtend.php
@@ -26,9 +26,9 @@ class LoginExtend
     {
         $adminsRepository = new AdminsRepository(); // 实例化AdminsRepository类
         $noMenusRoute = $adminsRepository::noMenusRoute(); // 获取不需要验证的路由
-        if(!in_array($api, $noMenusRoute)){
-            $superAdministratorIds = $adminsRepository::superAdministratorIds();// 获取超级管理员编号
-            if(!in_array($id, $superAdministratorIds)){// 不是超级管理员
+        $superAdministratorIds = $adminsRepository::superAdministratorIds();// 获取超级管理员编号
+        if(!in_array($id, $superAdministratorIds)){// 验证超级管理员
+            if(!in_array($api, $noMenusRoute)){ // 验证路由
                 $bool = $adminsRepository::returnAdminRuleMenusRoutes($id, $api); // 判断当前管理员是否有权限访问
                 return $bool;
             }
