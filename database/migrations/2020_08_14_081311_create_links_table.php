def1480447f9325bd76dd4b4dd0155ed5891ff56
cc_forever <1253705861@qq.com>
2020-08-24 11:44:56 +0800

    '合作伙伴和友情链接添加是否权重传递字段'

--- database/migrations/2020_08_14_081311_create_links_table.php
+++ database/migrations/2020_08_14_081311_create_links_table.php
@@ -30,10 +30,12 @@ class CreateLinksTable extends Migration
             $table->string('link', 128)->comment('友情链接');
             $table->string('image', 128)->comment('友情链接图片');
             $table->integer('weight')->comment('友情链接权重');
+            $table->tinyInteger('follow')->comment('是否权重传递 1 是 0 否');
             $table->integer('add_time')->comment('添加时间');
             $table->tinyInteger('is_del')->comment('是否删除(1是 0否)');
             $table->unique('id'); // 编号唯一索引
             $table->index('weight'); // 友情链接权重普通索引
+            $table->index('follow'); // 是否权重传递普通索引
             $table->index('is_del'); // 是否删除普通索引
         });
     }
--- database/migrations/2020_08_17_064625_create_partners_table.php
+++ database/migrations/2020_08_17_064625_create_partners_table.php
@@ -30,10 +30,12 @@ class CreatePartnersTable extends Migration
             $table->string('link', 128)->comment('合作伙伴链接');
             $table->string('image', 128)->comment('合作伙伴图片');
             $table->integer('weight')->comment('合作伙伴权重');
+            $table->tinyInteger('follow')->comment('是否权重传递 1 是 0 否');
             $table->integer('add_time')->comment('添加时间');
             $table->tinyInteger('is_del')->comment('是否删除(1是 0否)');
             $table->unique('id'); // 编号唯一索引
             $table->index('weight'); // 合作伙伴权重普通索引
+            $table->index('follow'); // 是否权重传递普通索引
             $table->index('is_del'); // 是否删除普通索引
         });
     }
